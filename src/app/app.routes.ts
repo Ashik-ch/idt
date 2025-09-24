25eaf2840be91da5c385d85d2f398f3ab495fd70
Ashik-ch <ashikch7117@gmail.com>
2025-09-24 22:15:48 +0530

    feat: add inbound-view component with routing and basic structure; update app routes to include inbound children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./components/home/home').then(m => m.Home),
-  },
+  { path: '', loadComponent: () => import('./components/home/home').then(m => m.Home), },
   {
     path: 'inbound',
-    loadComponent: () => import('./components/inbound/inbound').then(m => m.Inbound),
+    children: [
+      { path: '', loadComponent: () => import('./components/inbound/inbound').then(m => m.Inbound), },
+      { path: ':state', loadComponent: () => import('./components/inbound/inbound-view/inbound-view').then(m => m.InboundView), }
+    ]
   },
   {
-    path: 'outbound',
-    loadComponent: () => import('./components/outbound/outbound/outbound').then(m => m.Outbound),
+    path: 'outbound', loadComponent: () => import('./components/outbound/outbound/outbound').then(m => m.Outbound),
   },
 ];
---
+++ src/app/components/inbound/inbound-view/inbound-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inbound-view',
+  imports: [],
+  templateUrl: './inbound-view.html',
+  styleUrl: './inbound-view.css'
+})
+export class InboundView {
+
+}
