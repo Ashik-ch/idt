af5cd4a486899843144f45c6a96ef5eb2009364c
Ashik-ch <ashikch7117@gmail.com>
2025-09-24 15:24:37 +0530

    Enhance home component with new layout and background image; add multiple image assets

--- src/app/components/homeComponent/home/home.ts
+++ src/app/components/homeComponent/home/home.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { features } from '../../../data/data';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  features = features;
 
 }
