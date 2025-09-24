af363d57cd9d3d4910d07f0d724afdf9fde9f2e0
Ashik-ch <ashikch7117@gmail.com>
2025-09-24 21:56:50 +0530

    oubound map

--- src/app/components/outbound/outbound/outbound.ts
+++ src/app/components/outbound/outbound/outbound.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { OutboundMap } from '../outbound-map/outbound-map';
 
 @Component({
   selector: 'app-outbound',
-  imports: [],
+  imports: [
+    CommonModule, FormsModule,
+    OutboundMap
+  ],
   templateUrl: './outbound.html',
   styleUrl: './outbound.css'
 })
