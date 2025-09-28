b13472fe3ece01a3e72b62917872a1240fe0b4f8
Ashik-ch <ashikch7117@gmail.com>
2025-09-29 02:32:51 +0530

    animation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import * as AOS from 'aos';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// Initialize AOS
+AOS.init({
+  duration: 1000,
+  easing: 'ease-in-out',
+  once: true,
+  mirror: false
+});
